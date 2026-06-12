00277df56a0ffa41f7fec81d978d4a977f036929
dongwooBae <dw5817@naver.com>
2026-06-12 23:30:18 +0900

    1

--- lib/music.config.ts
+++ lib/music.config.ts
@@ -1,4 +1,4 @@
 export const TRACKS: { title: string; src: string }[] = [
-  { title: "배경음악 1", src: "/music/public/music/Alan_Walker_-_Faded_Vocal_Mix_(mp3.pm).mp3" },
+  { title: "배경음악 1", src: "/public/music/Alan_Walker_-_Faded_Vocal_Mix_(mp3.pm).mp3" },
   //{ title: "배경음악 2", src: "/music/track2.mp3" },
 ];
