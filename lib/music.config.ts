642dadbfa1f78a8d0d1691d6413bb773e8ad7db1
dongwooBae <dw5817@naver.com>
2026-06-13 00:19:55 +0900

    chore: 노래 추가 총 3개

--- lib/music.config.ts
+++ lib/music.config.ts
@@ -1,4 +1,5 @@
 export const TRACKS: { title: string; src: string }[] = [
   { title: "Amazing Grace", src: "/music/Amazing-Grace-2011(chosic.com).mp3" },
+  { title: "GrandFather's Old Clock", src: "/music/public/music/My Grandfather_s Clock (Piano & Celli).mp3_; filename_=UTF-8''My Grandfather_s Clock (Piano & Celli).mp3" },
   { title: "Faded", src: "/music/Alan_Walker_-_Faded_Vocal_Mix_(mp3.pm).mp3" },  
 ];
